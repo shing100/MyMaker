bdc88fe7399937fa707a122b65507aedd2ac4192
glim <shing100@naver.com>
2025-01-27 16:54:05 +0900

    Feat: Navigation Menu 2

---
+++ README-KO.md
@@ -0,0 +1,30 @@
+# My Maker
+이 프로젝트는 React Router를 사용하여 전체 스택 React 애플리케이션을 구축하기 위한 현대적이고 생산 준비가 완료된 템플릿입니다. 이 템플릿은 서버 측 렌더링, 핫 모듈 교체, 자산 번들링 및 최적화, 데이터 로딩 및 변형, TypeScript 기본 제공, TailwindCSS를 통한 스타일링 등의 기능을 제공합니다.
+
+## 환경 설정
+- 사용된 환경
+    - Tools
+        - Cursor
+    - Language
+        - TypeScript
+    - Framework & Library
+        - React
+        - React Router@7.0.1
+        - TailwindCSS [https://tailwindcss.com/docs/installation]
+        - Shadcn/UI [https://ui.shadcn.com/docs/components/separator]
+
+### 의존성 설치
+
+- 프로젝트 생성
+    - npx create-react-router@7.0.1
+
+- 프로젝트 설정
+    - 불필요한 docker 설정 제거
+    - 불필요한 파일 제거
+
+- 프로젝트 구조
+    - 프로젝트 구조 설정
+        - react-router.config.ts
+        - components.js
+
+ex) npx shadcn@latest add separator
--- app/common/components/navigation.tsx
+++ app/common/components/navigation.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router";
 import { Separator } from "~/common/components/ui/separator";
-import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuList, NavigationMenuTrigger } from "./ui/navigation-menu";
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger, navigationMenuTriggerStyle } from "./ui/navigation-menu";
+import { cn } from "~/lib/utils";
 
 const menus = [
     {
@@ -94,8 +95,6 @@ const menus = [
     {
         name: "IdeasGPT",
         to: "/ideas",
-        items: [
-        ]
     },
     {
         name: "Teams",
@@ -127,19 +126,42 @@ export default function Navigation() {
                     <NavigationMenuList>
                         {menus.map((menu) => (
                             <NavigationMenuItem key={menu.name}>
-                                <NavigationMenuTrigger>{menu.name}</NavigationMenuTrigger>
-                                <NavigationMenuContent>
-                                    {menu.items?.map((item) => (
-                                        <NavigationMenuItem key={item.name}>
-                                            <Link to={item.to}>{item.name}</Link>
-                                        </NavigationMenuItem>
-                                    ))}
-                                </NavigationMenuContent>
+                                {menu.items ? (
+                                    <>
+                                        <Link to={menu.to}>
+                                            <NavigationMenuTrigger>{menu.name}</NavigationMenuTrigger>
+                                        </Link>
+                                        <NavigationMenuContent>
+                                            <ul className="grid w-[600px] font-light gap-3 p-4 grid-cols-2">
+                                                {menu.items?.map((item) => (
+                                                    <NavigationMenuItem key={item.name}
+                                                        className={cn([
+                                                            "select-none rounded-md transition-colors focus:bg-accent/50 hover:bg-accent/50",
+                                                            item.to === "/products/promote" && "col-span-2 bg-primary/10 hover:bg-primary/20 focus:bg-primary/20",
+                                                            item.to === "/jobs/submit" && "col-span-2 bg-primary/10 hover:bg-primary/20 focus:bg-primary/20",
+                                                        ])}
+                                                    >
+                                                        <NavigationMenuLink asChild>
+                                                            <Link
+                                                                className="p-3 space-y-1 block leading-none no-underline outline-none"
+                                                                to={item.to} >
+                                                                <span className="text-sm font-medium leading-none">{item.name}</span>
+                                                                <p className="text-sm leading-snug text-muted-foreground">
+                                                                    {item.description}
+                                                                </p>
+                                                            </Link>
+                                                        </NavigationMenuLink>
+                                                    </NavigationMenuItem>
+                                                ))}
+                                            </ul>
+                                        </NavigationMenuContent>
+                                    </>
+                                ) : (<Link className={navigationMenuTriggerStyle()} to={menu.to}>{menu.name}</Link>)}
                             </NavigationMenuItem>
                         ))}
                     </NavigationMenuList>
-                </NavigationMenu>
-            </div>
-        </nav>
+                </NavigationMenu >
+            </div >
+        </nav >
     );
 }
