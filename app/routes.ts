3efe025e502faade56f4eb683a5dd32f0348178d
glim <shing100@naver.com>
2025-01-29 17:29:44 +0900

    feat: add Layout routes, navlink

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,4 @@
-import { type RouteConfig, index, prefix, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, prefix, route } from "@react-router/dev/routes";
 
 export default [
     index("common/pages/home-page.tsx"),
@@ -21,10 +21,12 @@ export default [
         route("/promote", "features/products/pages/promote-page.tsx"),
         ...prefix("/:productId", [
             index("features/products/pages/product-redirect-page.tsx"),
-            route("/overview", "features/products/pages/product-overview-page.tsx"),
-            ...prefix("/reviews", [
-                index("features/products/pages/product-reviews-page.tsx"),
-                route("/new", "features/products/pages/new-product-review-page.tsx"),
+            layout("features/products/layouts/product-overview-layout.tsx", [
+                route("/overview", "features/products/pages/product-overview-page.tsx"),
+                ...prefix("/reviews", [
+                    index("features/products/pages/product-reviews-page.tsx"),
+                    route("/new", "features/products/pages/new-product-review-page.tsx"),
+                ]),
             ]),
         ])
     ])
